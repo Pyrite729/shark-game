b2059c4af91a0790df17d462f150543c787147f4
Pyrite729 <lizzieknight800@gmail.com>
2026-08-26 22:11:08 +0100

    added difficulties

--- main game.py
+++ main game.py
@@ -4,6 +4,7 @@ import Buttons
 from Buttons import Button
 import math
 import random
+import time
 
 pygame.init()
 screen_w = (1080)
@@ -14,6 +15,7 @@ running = True
 animation_plays = False
 clock = pygame.time.Clock()
 FPS = 30
+scroll_speed = 4
 black = (0, 0, 0)
 lane = random.randint(0,2)
 obsticle_spawn_time = pygame.time.get_ticks()
@@ -24,19 +26,52 @@ obsticle_spawning_cooldown = random.randint(500, 1500)
 # image load -----------------------------------------------------------
 sprite_sheet_image = pygame.image.load("sprite walk.png").convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-start_button = pygame.image.load("start button.png").convert_alpha()
+
 background = pygame.image.load("racing lanes.png").convert_alpha()
+
 rock_1 = pygame.image.load("stone 1.png").convert_alpha()
 rock_2 = pygame.image.load("stone 2.png").convert_alpha()
 rock_3 = pygame.image.load("stone 3.png").convert_alpha()
+
+easy_button = pygame.image.load("easy button.png").convert_alpha()
+light_easy_button = pygame.image.load("light easy button.png").convert_alpha()
+
+medium_button = pygame.image.load("medium button.png").convert_alpha()
+light_start_button = pygame.image.load("light start button.png").convert_alpha()
+
+hard_button = pygame.image.load("hard button.png").convert_alpha()
+light_hard_button = pygame.image.load("light hard button.png").convert_alpha()
+
+left_button = pygame.image.load ("left button.png").convert_alpha()
+right_button = pygame.image.load ("right button.png").convert_alpha()
+
+light_left_button = pygame.image.load ("light left button.png").convert_alpha()
+light_right_button = pygame.image.load ("light right button.png").convert_alpha()
+
+big_left_button = pygame.image.load ("big left button.png").convert_alpha()
+big_right_button = pygame.image.load ("big right button.png").convert_alpha()
+
+sign = pygame.image.load("sighn.png").convert_alpha()
+sighn_eyes = pygame.image.load("sighn eyes.png").convert_alpha()
+
+epp_shark_card = pygame.image.load ("epp shark card.png").convert_alpha()
+
 background_w = background.get_width()
 background_h = background.get_height()
-start_button_interact = Button(0, 0, start_button, 1)
+
 # image load end -------------------------------------------------------
+
+# button interacts -----------------------------------------------------
+easy_button_interact = Button(290, 580, easy_button, 1)
+medium_button_interact = Button(472, 580, medium_button, 1)
+hard_button_interact = Button(654, 580, hard_button, 1)
+left_button_interact = Button(80, 360, left_button, 1)
+right_button_interact = Button(910, 360, right_button, 1)
+# button interacts end -------------------------------------------------
+
 
 # scroll ---------------------------------------------------------------
 scroll = 0
-scroll_speed = 4
 tiles = math.ceil(screen_h / background_h) +1
 print (tiles)
 # scroll end -----------------------------------------------------------
@@ -78,23 +113,6 @@ class Obsticles:
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-##class Lane_1_Rocks(Obsticles):
-##    def __init__(self,image):
-##        self.type = random.randint(0,2)
-##        super().__init__(image, self.type)
-##        self.rect.x = 321
-
-##class Lane_2_Rocks(Obsticles):
-##    def __init__(self,image):
-##        self.type = random.randint(0,2)
-##        super().__init__(image, self.type)
-##        self.rect.x = 555
-
-##class Lane_3_Rocks(Obsticles):
-##    def __init__(self,image):
-##        self.type = random.randint(0,2)
-##        super().__init__(image, self.type)
-##        self.rect.x = 770
 
 def spawn_obsticle():
     rocks_per_lane = [0, 0, 0]
@@ -126,11 +144,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     clock.tick(FPS)
-    
-    if start_button_interact.draw(screen) == True:
+
+    screen.fill((87, 132, 191))
+
+    screen.blit(sign, (268, 20))
+         
+    if left_button_interact.draw(screen) == True:
+        screen.blit(big_left_button, (62, 340))
+
+    if right_button_interact.draw(screen) == True:
+        screen.blit (big_right_button, (910, 340))
+
+    if easy_button_interact.draw(screen) == True:
         animation_plays = True
+        scroll_speed = 2
+
+    if medium_button_interact.draw(screen) == True:
+        animation_plays = True
+        scroll_speed = 4
+
+    if hard_button_interact.draw(screen) == True:
+        animation_plays = True
+        scroll_speed = 6
 
     if animation_plays:
         current_time = pygame.time.get_ticks()
@@ -143,7 +179,7 @@ while running:
         for i in range (-1, tiles):
             screen.blit(background, (0,i * background_h + scroll))
 
-        scroll = scroll + 5
+        scroll = scroll + (scroll_speed)
 
         if scroll >= background_h:
             scroll = 0
@@ -165,6 +201,8 @@ while running:
         ##  if player.shark_rect.colliderect(obsticle.rect):
         ##      pygame.draw.rect(screen, (255, 0, 0), player.shark_rect, 2)
         screen.blit(animation_list[frame], (476, 592))
+
+##    pygame.draw.rect(screen, (255, 255, 255), easy_button_interact, 2)
 
     pygame.display.update()
 
