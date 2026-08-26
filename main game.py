bf1e8c6b652f9083d8c4997a2304f2d04ee232a6
Pyrite729 <lizzieknight800@gmail.com>
2026-08-26 16:56:30 +0100

    added rock spawns

--- main game.py
+++ main game.py
@@ -3,6 +3,7 @@ import spritesheet
 import Buttons
 from Buttons import Button
 import math
+import random
 
 pygame.init()
 screen_w = (1080)
@@ -14,6 +15,9 @@ animation_plays = False
 clock = pygame.time.Clock()
 FPS = 30
 black = (0, 0, 0)
+lane = random.randint(0,2)
+obsticle_spawn_time = pygame.time.get_ticks()
+obsticle_spawning_cooldown = random.randint(500, 1500)
 # end of init stuff ------------------------------------------------
 
 
@@ -22,6 +26,9 @@ sprite_sheet_image = pygame.image.load("sprite walk.png").convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
 start_button = pygame.image.load("start button.png").convert_alpha()
 background = pygame.image.load("racing lanes.png").convert_alpha()
+rock_1 = pygame.image.load("stone 1.png").convert_alpha()
+rock_2 = pygame.image.load("stone 2.png").convert_alpha()
+rock_3 = pygame.image.load("stone 3.png").convert_alpha()
 background_w = background.get_width()
 background_h = background.get_height()
 start_button_interact = Button(0, 0, start_button, 1)
@@ -29,6 +36,7 @@ start_button_interact = Button(0, 0, start_button, 1)
 
 # scroll ---------------------------------------------------------------
 scroll = 0
+scroll_speed = 4
 tiles = math.ceil(screen_h / background_h) +1
 print (tiles)
 # scroll end -----------------------------------------------------------
@@ -45,7 +53,62 @@ for x in range(animation_steps):
 
 
 # obsticles -------------------------------------------------------------
-rock_group = pygame.sprite.Group()
+obsticles = []
+rock_images = [rock_1, rock_2, rock_3]
+
+class Obsticles:
+    def __init__(self, lane):
+        self.lane = lane
+        self.image = random.choice(rock_images)
+        self.rect = self.image.get_rect()
+        self.rect.y = -self.rect.height
+
+        if self.lane == 0:
+            self.rect.x = 241
+        elif self.lane == 1:
+            self.rect.x = 475
+        elif self.lane == 2:
+            self.rect.x = 700
+
+    def update(self):
+        self.rect.y += scroll_speed
+        if self.rect.y > screen_h:
+            obsticles.remove(self)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+##class Lane_1_Rocks(Obsticles):
+##    def __init__(self,image):
+##        self.type = random.randint(0,2)
+##        super().__init__(image, self.type)
+##        self.rect.x = 321
+
+##class Lane_2_Rocks(Obsticles):
+##    def __init__(self,image):
+##        self.type = random.randint(0,2)
+##        super().__init__(image, self.type)
+##        self.rect.x = 555
+
+##class Lane_3_Rocks(Obsticles):
+##    def __init__(self,image):
+##        self.type = random.randint(0,2)
+##        super().__init__(image, self.type)
+##        self.rect.x = 770
+
+def spawn_obsticle():
+    rocks_per_lane = [0, 0, 0]
+    for obsticle in obsticles:
+        rocks_per_lane[obsticle.lane] += 1
+    available_lanes = []
+    for lane in range(3):
+        if rocks_per_lane[lane] < 2:
+            available_lanes.append(lane)
+    if len(available_lanes) > 0:
+        lane = random.choice(available_lanes)
+        obsticles.append(Obsticles(lane))
+
+
 # obsticles end ---------------------------------------------------------
 
 
@@ -78,6 +141,22 @@ while running:
         if scroll >= background_h:
             scroll = 0
 
+
+        current_time = pygame.time.get_ticks()
+
+        if current_time - obsticle_spawn_time >= obsticle_spawning_cooldown:
+            spawn_obsticle()
+            obsticle_spawn_time = current_time
+            obsticle_spawning_cooldown = random.randint(500, 1500)
+
+##        if len(obsticles) == 0:
+##            obsticles.append(Obsticles())
+
+        for obsticle in obsticles:
+            obsticle.draw(screen)
+            obsticle.update()
+        ##  if player.shark_rect.colliderect(obsticle.rect):
+        ##      pygame.draw.rect(screen, (255, 0, 0), player.shark_rect, 2)
         screen.blit(animation_list[frame], (476, 592))
 
     pygame.display.update()
--- rocks.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-
-class Rocks:
-    def __init__(self, x, y, image):
-        width = image.get_width()
-        height = image.get_height()
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
